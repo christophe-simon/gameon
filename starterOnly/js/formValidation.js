36c7fae2bdd7b7432c0daa96ba4dea12eaa319b7
Christophe Simon <csimon.developpeur@gmail.com>
2022-04-26 15:17:50 +0200

    Update and finish project
    Put the elements of the validation modal and the error messages in HTML and then use JS accordingly to display or delete error messages or display validation modal if all the elements are correctly entered

--- starterOnly/js/formValidation.js
+++ starterOnly/js/formValidation.js
@@ -14,346 +14,69 @@ const competitionsNumberRegex = new RegExp(COMPETITIONS_NUMBER_REGEX);
 const LOCATION_REGEX = /^New York|San Francisco|Seattle|Chicago|Boston|Portland$/;
 const locationRegex = new RegExp(LOCATION_REGEX);
 
-// Error messages
-const NAME_ERROR = 'Vous devez indiquer un nom contenant un minimum de 2 caractères';
-const EMAIL_ERROR = 'Vous devez indiquer une adresse email valide';
-const DATE_ERROR = 'Vous devez indiquer une date correcte';
-const QUANTITY_ERROR = 'Vous devez indiquer un nombre compris entre 0 et 99';
-const LOCATION_ERROR = 'Vous devez sélectioner une ville';
-const AGREEMENT_ERROR = 'Vous devez accepter les conditions d\'utilisation';
-
 /**
- * Checks if the first name is valid (ie. contains at least 2 characters)
+ * Checks if a data is valid
+ * @param {string} elt 
  * @returns {boolean}
  */
-/*let isFirstNameValid = function() {
-    const firstNameElt = document.getElementById('first');
-    if (nameRegex.test(firstNameElt.value)) {
-        firstNameElt.parentElement.setAttribute('data-error-visible', 'false');
-        return true;
-    } else {
-        firstNameElt.parentElement.setAttribute('data-error-visible', 'true');
-        
-        displayOrHideErrorMessage(firstNameElt, NAME_ERROR);
-
-        return false;
-    }
-}*/
-
-
-
 let isDataValid = function(elt) {
     switch (elt) {
         case 'firstNameElt':
-            const firstNameElt = document.getElementById('first');
             return nameRegex.test(firstNameElt.value);
         case 'lastNameElt':
-            const lastNameElt = document.getElementById('last');
             return nameRegex.test(lastNameElt.value);
         case 'emailElt':
-            const emailElt = document.getElementById('email');
             return emailRegex.test(emailElt.value);
         case 'birthdateElt':
-            const birthdateElt = document.getElementById('birthdate');
             return dateRegex.test(birthdateElt.value);
         case 'quantityElt':
-            const quantityElt = document.getElementById('quantity');
             return competitionsNumberRegex.test(quantityElt.value);
         case 'locationElt':
             const chosenLocationElt = document.querySelector('input[type="radio"]:checked');
             return (chosenLocationElt !== null && locationRegex.test(chosenLocationElt.value));
         case 'agreementElt':
-            const agreementElt = document.getElementById('checkbox1');
             return agreementElt.checked;
         default:
-            console.log('Pas de donnée de ce type');
+            throw 'Pas de donnée de ce type';
     }
 }
 
-/*let manageErrorMessageDisplay = function(elt, errorMessage) {
-    if (elt !== null && isDataValid(elt)) {
-
-    } else {
-        const newErrorMessageElt = document.createElement('p');
-        newErrorMessageElt.classList.add('error-message');
-        newErrorMessageElt.textContent = errorMessage;
-        elt.parentElement.appendChild(newErrorMessageElt);
-    }
-
-}*/
-
-let manageElementdisplay = function(elt) {
-
-    // 1ère variante
-    
+/**
+ * Display correctly (in casual mode or in error mode) an element whether its value is valid or not
+ * @param {string} elt 
+ */
+let manageElementDisplay = function(elt) {
     let elements = {
-        firstNameElt: document.getElementById('first'),
-        lastNameElt: document.getElementById('last'),
-        emailElt: document.getElementById('email'),
-        birthdateElt: document.getElementById('birthdate'),
-        quantityElt: document.getElementById('quantity'),
-        locationElt: document.querySelector('input[type="radio"]:nth-child(1)'),
-        agreementElt: document.getElementById('checkbox1')
+        firstNameElt,
+        lastNameElt,
+        emailElt,
+        birthdateElt,
+        quantityElt,
+        locationElt,
+        agreementElt
     };
 
-    let elementsToDelete = {
-        firstNameElt: document.querySelector('#first + p.error-message'),
-        lastNameElt: document.querySelector('#last + p.error-message'),
-        emailElt: document.querySelector('#email + p.error-message'),
-        birthdateElt:document.querySelector('#birthdate + p.error-message'),
-        quantityElt: document.querySelector('#quantity + p.error-message'),
-        locationElt: document.querySelector('.checkbox-label[for="location6"] + p.error-message'),
-        agreementElt: document.querySelector('.checkbox2-label + p.error-message')
-    };
-
-    let errorMessages = {
-        firstNameElt: NAME_ERROR,
-        lastNameElt: NAME_ERROR,
-        emailElt: EMAIL_ERROR,
-        birthdateElt: DATE_ERROR,
-        quantityElt: QUANTITY_ERROR,
-        locationElt: LOCATION_ERROR,
-        agreementElt: AGREEMENT_ERROR
+    let errorMessagesElts = {
+        firstNameElt: firstNameErrorMessageElt,
+        lastNameElt: lastNameErrorMessageElt,
+        emailElt: emailErrorMessageElt,
+        birthdateElt: birthdateErrorMessageElt,
+        quantityElt: quantityErrorMessageElt,
+        locationElt: locationErrorMessageElt,
+        agreementElt: agreementErrorMessageElt
     };
 
     if (elt !== null && isDataValid(elt)) {
-        if (elementsToDelete[elt] !== null) {
-            (elements[elt]).parentElement.removeChild(elementsToDelete[elt]);
-        }
-        (elements[elt]).parentElement.setAttribute('data-error-visible', 'false');
+        elements[elt].parentElement.setAttribute('data-error-visible', 'false');
+        errorMessagesElts[elt].style.display = 'none';
     } else {
-        if (elt == 'agreementElt') {
-            console.log(elt);
-            console.log(elements[elt]);
-            console.log(elementsToDelete[elt]);
-        }
-
-        if (elementsToDelete[elt] === null) {
-            const newErrorMessageElt = document.createElement('p');
-            newErrorMessageElt.classList.add('error-message');
-            newErrorMessageElt.textContent = errorMessages[elt];
-            (elements[elt]).parentElement.appendChild(newErrorMessageElt);
-        }
         (elements[elt]).parentElement.setAttribute('data-error-visible', 'true');
+        errorMessagesElts[elt].style.display = 'block';
     }
-
-
-    //2ème version
- /*
-    if (elt !== null && isDataValid(elt)) {
-
-        let elementToDelete;
-
-        switch (elt) {
-            case 'firstNameElt':
-                elementToDelete = document.querySelector('#first + p.error-message');
-                if (elementToDelete !== null) {
-                    document.getElementById('first').parentElement.removeChild(elementToDelete);
-                }
-                document.getElementById('first').parentElement.setAttribute('data-error-visible', 'false');
-                break;
-            case 'lastNameElt':
-                elementToDelete = document.querySelector('#last + p.error-message');
-                if (elementToDelete !== null) {
-                    document.getElementById('last').parentElement.removeChild(elementToDelete);
-                }
-                document.getElementById('last').parentElement.setAttribute('data-error-visible', 'false');
-                break;
-            case 'emailElt':
-                elementToDelete = document.querySelector('#email + p.error-message');
-                if (elementToDelete !== null) {
-                    document.getElementById('email').parentElement.removeChild(elementToDelete);
-                }
-                document.getElementById('email').parentElement.setAttribute('data-error-visible', 'false');
-                break;
-            case 'birthdateElt':
-                elementToDelete = document.querySelector('#birthdate + p.error-message');
-                if (elementToDelete !== null) {
-                    document.getElementById('birthdate').parentElement.removeChild(elementToDelete);
-                }
-                document.getElementById('birthdate').parentElement.setAttribute('data-error-visible', 'false');
-                break;
-            case 'quantityElt':
-                elementToDelete = document.querySelector('#quantity + p.error-message');
-                if (elementToDelete !== null) {
-                    document.getElementById('quantity').parentElement.removeChild(elementToDelete);
-                }
-                document.getElementById('quantity').parentElement.setAttribute('data-error-visible', 'false');
-                break;
-            case 'locationElt':
-                elementToDelete = document.querySelector('#location6 + p.error-message');
-                if (elementToDelete !== null) {
-                    document.getElementById('location6').parentElement.removeChild(elementToDelete);
-                }
-                document.querySelector('input[type="radio"]:nth-child(1)').parentElement.setAttribute('data-error-visible', 'false');
-                break;
-            case 'agreementElt':
-                elementToDelete = document.querySelector('#checkbox2 + p.error-message');
-                if (elementToDelete !== null) {
-                    document.getElementById('checkbox2').parentElement.removeChild(elementToDelete);
-                }
-                document.getElementById('checkbox1').parentElement.setAttribute('data-error-visible', 'false');
-                break;
-            default:
-                console.log('elt ne correspond à aucune valeur');
-        }
-    } else {
-        const newErrorMessageElt = document.createElement('p');
-        newErrorMessageElt.classList.add('error-message');
-        switch (elt) {
-            case 'firstNameElt':
-                document.getElementById('first').parentElement.setAttribute('data-error-visible', 'true');          
-                newErrorMessageElt.textContent = NAME_ERROR;
-                document.getElementById('first').parentElement.appendChild(newErrorMessageElt);
-                break;
-            case 'lastNameElt':
-                document.getElementById('last').parentElement.setAttribute('data-error-visible', 'true');
-                newErrorMessageElt.textContent = NAME_ERROR;
-                document.getElementById('last').parentElement.appendChild(newErrorMessageElt);
-                break;
-            case 'emailElt':
-                document.getElementById('email').parentElement.setAttribute('data-error-visible', 'true');
-                newErrorMessageElt.textContent = EMAIL_ERROR;
-                document.getElementById('email').parentElement.appendChild(newErrorMessageElt);
-                break;
-            case 'birthdateElt':
-                document.getElementById('birthdate').parentElement.setAttribute('data-error-visible', 'true');
-                newErrorMessageElt.textContent = DATE_ERROR;
-                document.getElementById('birthdate').parentElement.appendChild(newErrorMessageElt);
-                break;
-            case 'quantityElt':
-                document.getElementById('quantity').parentElement.setAttribute('data-error-visible', 'true');
-                newErrorMessageElt.textContent = QUANTITY_ERROR;
-                document.getElementById('quantity').parentElement.appendChild(newErrorMessageElt);
-                break;
-            case 'locationElt':
-                document.querySelector('input[type="radio"]:nth-child(1)').parentElement.setAttribute('data-error-visible', 'true');
-                newErrorMessageElt.textContent = LOCATION_ERROR;
-                document.querySelector('input[type="radio"]:nth-child(1)').parentElement.appendChild(newErrorMessageElt);
-                break;
-            case 'agreementElt':
-                document.getElementById('checkbox1').parentElement.setAttribute('data-error-visible', 'true');
-                newErrorMessageElt.textContent = AGREEMENT_ERROR;
-                document.getElementById('checkbox1').parentElement.appendChild(newErrorMessageElt);
-                break;
-            default:
-                console.log('elt ne correspond à aucune valeur');
-        }
-    }*/
 }
 
-/*let manageFirstNameDisplay = function() {
-    if (isDataValid('firstNameElt')) {
-        (document.getElementById('first')).parentElement.setAttribute('data-error-visible', 'false');
-    } else {
-        (document.getElementById('first')).parentElement.setAttribute('data-error-visible', 'true');
-    }
-}*/
-
-
-
-/*let deleteErrorMessage = function(errorElt) {
-    const errorMessageArray = document.getElementsByClassName('error-message');
-    if (errorMessageArray !== null) {
-        let error
-    }
-    errorElement.parentElement.removeChild(docum)
-    elt.removeChild(newElt);
-}*/
-
 /**
- * Checks if the last name is valid (ie. contains at least 2 characters)
- * @returns {boolean}
- */
-/*let isLastNameValid = function() {
-    const lastNameElt = document.getElementById('last');
-    if (nameRegex.test(lastNameElt.value)) {
-        lastNameElt.parentElement.setAttribute('data-error-visible', 'false');
-        return true;
-    } else {
-        lastNameElt.parentElement.setAttribute('data-error-visible', 'true');
-        return false;
-    }
-}*/
-
-/**
- * Checks if the email address is valid
- * @returns {boolean}
- */
-/*let isEmailValid = function() {
-    const emailElt = document.getElementById('email');
-    if (emailRegex.test(emailElt.value)) {
-        emailElt.parentElement.setAttribute('data-error-visible', 'false');
-        return true;
-    } else {
-        emailElt.parentElement.setAttribute('data-error-visible', 'true');
-        return false;
-    }
-}*/
-
-/**
- * Checks if the birth date is valid
- * @returns {boolean}
- */
-/*let isBirthDateValid = function() {
-    const birthdateElt = document.getElementById('birthdate');
-    if (dateRegex.test(birthdateElt.value)) {
-        birthdateElt.parentElement.setAttribute('data-error-visible', 'false');
-        return true;
-    } else {
-        birthdateElt.parentElement.setAttribute('data-error-visible', 'true');
-        return false;
-    }
-}*/
-
-/**
- * Checks if the number of competitions is valid (ie. between 0 and 99)
- * @returns {boolean}
- */
-/*let isCompetitionsNumberValid = function() {
-    const quantityElt = document.getElementById('quantity');
-    if (competitionsNumberRegex.test(quantityElt.value)) {
-        quantityElt.parentElement.setAttribute('data-error-visible', 'false');
-        return true;
-    } else {
-        quantityElt.parentElement.setAttribute('data-error-visible', 'true');
-        return false;
-    }
-}*/
-
-/**
- * Checks if the location is valid (ie. if one of the towns is selected)
- * @returns {boolean}
- */
-/*let isLocationValid = function() {
-    const firstLocationElt = document.querySelector('input[type="radio"]:nth-child(1)');
-    const chosenLocationElt = document.querySelector('input[type="radio"]:checked');
-    if (chosenLocationElt !== null && locationRegex.test(chosenLocationElt.value)) {
-        firstLocationElt.parentElement.setAttribute('data-error-visible', 'false');
-        return true;
-    } else {
-        firstLocationElt.parentElement.setAttribute('data-error-visible', 'true');
-        return false;
-    }
-}*/
-
-/**
- * Checks if the agreement is valid (ie. if the agreement checkbox is checked)
- * @returns {boolean}
- */
-/*let isAgreementValid = function() {
-    const agreementElt = document.getElementById('checkbox1');
-    if (agreementElt.checked === true) {
-        agreementElt.parentElement.setAttribute('data-error-visible', 'false');
-        return true;
-    } else {
-        agreementElt.parentElement.setAttribute('data-error-visible', 'true');
-        return false;
-    }
-}*/
-
-/**
- * Checks if form is valid
+ * Checks if the form is valid
  * @returns {boolean}
  */
 let isFormValid = function() {
@@ -368,30 +91,22 @@ let isFormValid = function() {
     );
 }
 
-const form = document.getElementsByTagName('form')[0];
-form.addEventListener('submit', function(e) {
+/**
+ * Checks if the form is valid and display the validation modal in this case, or display the errors if there are any
+ */
+formElt.addEventListener('submit', function(e) {
     e.preventDefault();
 
     if (isFormValid()) {
-        closeModal();
-        createSuccessModal();
-        displaySuccessModal();
+        closeFormModal();
+        displayValidationModal();
     } else {
-        manageElementdisplay('firstNameElt');
-        manageElementdisplay('lastNameElt');
-        manageElementdisplay('emailElt');
-        manageElementdisplay('birthdateElt');
-        manageElementdisplay('quantityElt');
-        manageElementdisplay('locationElt');
-        manageElementdisplay('agreementElt');
+        manageElementDisplay('firstNameElt');
+        manageElementDisplay('lastNameElt');
+        manageElementDisplay('emailElt');
+        manageElementDisplay('birthdateElt');
+        manageElementDisplay('quantityElt');
+        manageElementDisplay('locationElt');
+        manageElementDisplay('agreementElt');
     }
 });
-
-
-
-
-
-
-
-
-
--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,87 +1,80 @@
 // DOM Elements
-const modalBg = document.querySelector('.bground');
+const formModalBg = document.getElementById('form-modal');
+const validationModalBg = document.getElementById('validation-modal');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
-const closeBtn1 = document.getElementsByClassName('close')[0];
-
+const formModalCloseBtn = document.getElementById('form-modal-close-btn');
+const validationModalCloseBtn1 = document.getElementById('validation-modal-close-btn-1');
+const validationModalCloseBtn2 = document.getElementById('validation-modal-close-btn-2');
+const errorMessages = document.getElementsByClassName('error-message');
+const formElt = document.querySelector('form[name="reserve"]');
+const firstNameElt = document.getElementById('first');
+const lastNameElt = document.getElementById('last');
+const emailElt = document.getElementById('email');
+const birthdateElt = document.getElementById('birthdate');
+const quantityElt = document.getElementById('quantity');
+const locationElt = document.querySelector('input[type="radio"]:nth-child(1)')
+const agreementElt = document.getElementById('checkbox1');
+const firstNameErrorMessageElt = document.querySelector('#first-name-data p.error-message');
+const lastNameErrorMessageElt = document.querySelector('#last-name-data p.error-message');
+const emailErrorMessageElt = document.querySelector('#email-data p.error-message');
+const birthdateErrorMessageElt = document.querySelector('#birthdate-data p.error-message');
+const quantityErrorMessageElt = document.querySelector('#quantity-data p.error-message');
+const locationErrorMessageElt = document.querySelector('#location-data p.error-message');
+const agreementErrorMessageElt = document.querySelector('#checkboxes-data p.error-message');
 
 
 /**
- * Launch modal form
+ * Launch form modal when you click on a 'Je m'inscris' button
  */
-function launchModal() {
-    modalBg.style.display = "block";
+function launchFormModal() {
+    formModalBg.style.display = 'block';
+    initializeFormModal();
+    validationModalBg.style.display = 'none';
 }
 
-modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchFormModal));
 
 /**
- * Close modal form
+ * Initialize the form modal
  */
-function closeModal() {
-    modalBg.style.display = "none";
+function initializeFormModal() {
+    for (message of errorMessages) {
+        message.style.display = 'none';
+    }
+    formElt.reset();
 }
 
-closeBtn1.addEventListener('click', closeModal);
-
-
-function createSuccessModal() {
-
-    // Let's target the main tag and add a child element: div.bground2
-    let mainEltArray = document.getElementsByTagName('main');
-    let mainElt = mainEltArray[0];
-
-    const newBgroundElt = document.createElement('div');
-    newBgroundElt.classList.add('bground2');
-
-    mainElt.appendChild(newBgroundElt);
-
-    // Let's add a child element: div.content
-    const newContentElt = document.createElement('div');
-    newContentElt.classList.add('content');
-
-    newBgroundElt.appendChild(newContentElt);
-
-    // Let's add 2 children elements: span.close and div.modal-body.modal-body-success
-    const newCloseElt = document.createElement('span');
-    newCloseElt.classList.add('close');
-
-    const newModalBodyElt = document.createElement('div');
-    newModalBodyElt.classList.add('modal-body');
-    newModalBodyElt.classList.add('modal-body-success');
-
-    newContentElt.appendChild(newCloseElt);
-    newContentElt.appendChild(newModalBodyElt);
-
-    // Let's add 2 children elements: p and button.btn-signup.modal-btn
-    const newMessageElt = document.createElement('p');
-    newMessageElt.textContent = 'Merci pour votre inscription';
-
-    const newCloseBtn = document.createElement('button');
-    newCloseBtn.textContent = 'Fermer';
-    newCloseBtn.classList.add('btn-signup');
-    newCloseBtn.classList.add('modal-btn');
-    newCloseBtn.classList.add('modal-btn-success');
-
-    newModalBodyElt.appendChild(newMessageElt);
-    newModalBodyElt.appendChild(newCloseBtn);
+/**
+ * Close form modal when you click on the cross in the top right corner of the form modal
+ */
+function closeFormModal() {
+    formModalBg.style.display = 'none';
 }
 
-function displaySuccessModal() {
-        const modalBgArray = document.getElementsByClassName('.bground2');
-        console.log(modalBgArray);
-        const successModalBg = modalBgArray[0];
-        console.log(successModalBg);
-        successModalBg.style.display = "block";
+formModalCloseBtn.addEventListener('click', closeFormModal);
+
+/**
+ * Display validation modal when you successfully submit the form
+ */
+function displayValidationModal() {
+    validationModalBg.style.display = 'block';
 }
 
-/*
-function closeSuccessModal() {
-    //const closeBtn2 = document.getElementsByClassName('close')[1];
-    const modalBgArray = document.getElementsByClassName('.bground');
-    const successModalBg = modalBgArray[1];
-    successModalBg.style.display = "none";
+/**
+ * Close validation modal when you click on the cross in the top right corner of the validation modal
+ */
+ function closeValidationModal1() {
+    validationModalBg.style.display = 'none';
 }
 
-(document.getElementsByClassName('close')[1]).addEventListener('click', closeSuccessModal);
-*/
+validationModalCloseBtn1.addEventListener('click', closeValidationModal1);
+
+/**
+ * Close validation modal when you click on the 'close' button of the validation modal
+ */
+ function closeValidationModal2() {
+    validationModalBg.style.display = 'none';
+}
+
+validationModalCloseBtn2.addEventListener('click', closeValidationModal2);
